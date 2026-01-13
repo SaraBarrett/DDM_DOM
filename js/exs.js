2a00451eedbafb3fe36e0f96c92c84254d202162
SaraBarrett <sara.c.mon@gmail.com>
2026-01-13 08:58:00 +0000

    manipulação

--- js/exs.js
+++ js/exs.js
@@ -1,4 +1,67 @@
-const image = document.getElementById("unicorn");
-console.log(image);
-const heading = document.getElementById("mainheading");
-console.log(heading);
+// const image = document.getElementById("unicorn");
+// console.log(image);
+// const heading = document.getElementById("mainheading");
+// console.log(heading);
+
+//pp12
+// let doneTodos = document.querySelectorAll('.done');
+
+// let checkbox = document.querySelector("input[type='checkbox']");
+
+// console.log(doneTodos)
+// console.log(checkbox)
+
+//pp15
+function changeTaste() {
+  let mySpan = document.querySelector("span");
+  if (mySpan.textContent == "Yammi") {
+    mySpan.textContent = "Yack";
+  } else {
+    mySpan.textContent = "Yammi";
+  }
+}
+
+//pp 20
+
+function eggOrChicken() {
+  let myImg = document.querySelector("img");
+
+  if (
+    myImg.getAttribute("src") == "https://devsprouthosting.com/images/egg.jpg"
+  ) {
+    myImg.setAttribute(
+      "src",
+      "https://devsprouthosting.com/images/chicken.jpg"
+    );
+  } else {
+    myImg.setAttribute("src", "https://devsprouthosting.com/images/egg.jpg");
+  }
+}
+
+//pp 22
+function changeStyle() {
+  const alinhar = document.getElementById("container");
+  alinhar.style.textAlign = "center";
+
+  const image = document.querySelector("img");
+  image.style.width = "150px";
+  image.style.borderRadius = "50%";
+}
+
+//pp 24
+function makeRandom() {
+  let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+  let myraimbow = document.querySelectorAll("span");
+
+//   let count = 0;
+//   for (let raimbow of myraimbow) {
+//     raimbow.style.backgroundColor = colors[count];
+//  count++;
+//   }
+
+myraimbow.forEach((item, index) =>{
+   item.style.backgroundColor = colors[index];
+}
+)
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-const myBanner = document.getElementById('banner');
+function changeLang() {
+  //seleciona o elemento
+  let myH1 = document.querySelector("h1");
 
-console.log(myBanner.src)
+  //manipula o elemento
+  myH1.textContent = "Galinhas Sedosas";
+  myH1.style.backgroundColor = "green";
+}
+
+function changeImg() {
+  //sintaxe 1
+  const myBanner = document.getElementById("banner");
+  //myBanner.src ="https://istec-porto.pt/wp-content/uploads/2026/01/cropped-1.png";
+
+  //Sintaxe 2
+  myBanner.setAttribute("src", "../images/assesement.jpeg");
+}
+
+function changeAll() {
+  let myLis = document.querySelectorAll("li");
+  console.log(myLis);
+
+  for (element of myLis) {
+    element.style.backgroundColor = "blue";
+  }
+}
+
+function changeMode() {
+  let myBody = document.querySelector("body");
+
+  if (myBody.classList.contains("day")) {
+    myBody.classList.remove("day");
+    myBody.classList.add("night");
+  } else {
+    myBody.classList.remove("night");
+    myBody.classList.add("day");
+  }
+}
